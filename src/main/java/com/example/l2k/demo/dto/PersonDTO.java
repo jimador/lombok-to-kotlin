d3e4bfddaa731731c661529393a8f135cce434b5
James Dunnam <7660553+jimador@users.noreply.github.com>
2019-01-22 09:21:50 -0500

    adding lombok to dto

--- src/main/java/com/example/l2k/demo/dto/PersonDTO.java
+++ src/main/java/com/example/l2k/demo/dto/PersonDTO.java
@@ -1,66 +1,13 @@
 package com.example.l2k.demo.dto;
 
-import java.util.Objects;
+import lombok.Data;
 
 /**
  * @author James Dunnam
  */
+@Data
 public class PersonDTO {
     private final String name;
     private final int age;
     private String nickName;
-
-    public PersonDTO(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public PersonDTO() {
-        this.name = "Fred";
-        this.age = 101;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(getName(), personDTO.getName()) &&
-               Objects.equals(getAge(), personDTO.getAge()) &&
-               Objects.equals(getNickName(), personDTO.getNickName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getAge(), getNickName());
-    }
-
-    @Override
-    public String toString() {
-        return "PersonDTO{" +
-               "name='" + name + '\'' +
-               ", age='" + age + '\'' +
-               ", nickName='" + nickName + '\'' +
-               '}';
-    }
 }
